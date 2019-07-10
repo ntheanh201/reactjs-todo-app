521fd6e016b05ad7a6898459491f66263f0f2123
ntheanh201 <ntheanh201@gmail.com>
2019-07-10 22:23:03 +0700

    todoapp-state-management

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react';
+import Footer from './Layouts/Footer';
+import TodoList from './TodoList/TodoList';
+import styled from 'styled-components'
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Title>todos</Title>
+        <TodoList />
+        <Footer />
+      </Fragment>
+    );
+  }
+}
+
+const Title = styled.h1`
+    // position: absolute;
+	// top: -155px;
+	width: 100%;
+	font-size: 100px;
+	font-weight: 100;
+	text-align: center;
+	color: rgba(175, 47, 47, 0.15);
+	-webkit-text-rendering: optimizeLegibility;
+	-moz-text-rendering: optimizeLegibility;
+	text-rendering: optimizeLegibility;
+`;
+
+export default App;
---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react'
+import uuid from 'uuid'
+import Todos from './components/Todos';
+import styled from 'styled-components'
+import ActionBar from './components/ActionBar';
+import InputBar from './components/InputBar';
+const filters = [{
+        name: 'showAll',
+        filterTodos: (todos) => todos
+    }, {
+        name: 'showActive',
+        filterTodos: (todos) =>  todos.filter(({isDone}) => !isDone)
+    }, {
+        name: 'showCompleted',
+        filterTodos: (todos) =>  todos.filter(({isDone}) => isDone)
+    }]
+export default class TodoList extends Component {
+  
+    constructor(props) {
+        super(props);
+        const todos = [
+            {
+                id: 0,
+                isDone: false,
+                name: 'TodoItem 1'
+            },
+            {
+                id: 1,
+                isDone: true,
+                name: 'TodoItem 2'
+            }
+        ]
+        this.state = {
+            todos,
+            filteredTodos: todos,
+            count: 0,
+            toggleStatus: false,
+            filter: filters[0]
+        }
+    }
+
+    createTodo = (name) => {
+        const todos = [...this.state.todos, {id: uuid(), isDone: false, name}]
+        this.setState({
+            todos: todos,
+        }, this.filterTodos)
+        
+    }
+
+    updateTodo = (todo) => {
+        let { todos } = this.state
+        const index = todos.map(todo => todo.id).indexOf(todo.id)
+        todos[index] = todo
+        this.setState({
+            todos,
+        }, this.filterTodos)
+    }
+
+    getUndoneTodo = () => {
+        const { filteredTodos } = this.state
+        return filteredTodos.reduce((count, {isDone}) => !isDone ? count += 1: count, 0)
+    }
+
+    // showAll = () => {
+    //     const { todos } = this.state
+    //     this.setState({
+    //         filteredTodos: todos
+    //     })
+    // }
+
+    // showActive = () => {
+    //     let { todos } = this.state
+    //     this.setState({
+    //         filteredTodos: todos.filter(({isDone}) => !isDone)
+    //     })
+    // }
+
+    // showCompleted = () => {
+    //     const { todos } = this.state
+    //     this.setState({
+    //         filteredTodos: todos.filter(({isDone}) => isDone)
+    //     })
+    // }
+
+    filterTodos = () => {
+        const { filter } = this.state
+        const { todos } = this.state
+        this.setState({
+            filteredTodos: filter.filterTodos(todos)
+        })
+    }
+
+    toggleFilter = (index) => {
+        const currentFilter = filters[index]
+        this.setState({
+            filter: currentFilter
+        }, this.filterTodos)
+    }
+
+    clearCompletedTodo = () => {
+        let { todos } = this.state
+        this.setState({
+            todos: todos.filter(({isDone}) => !isDone) 
+        }, this.filterTodos)
+    }
+
+    toggleAllTodo = () => {
+        let { todos, toggleStatus } = this.state
+        this.setState({
+            todos: todos.map(({id, isDone, name}) => ({id, isDone: !toggleStatus, name})),
+            toggleStatus: !toggleStatus,
+        }, this.filterTodos)
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <InputBar 
+                createTodo={this.createTodo} 
+                toggleAllTodo={this.toggleAllTodo}
+                />
+                <Todos
+                updateTodo={this.updateTodo}
+                todos={this.state.filteredTodos} />
+                <ActionBar
+                clearCompletedClick={this.clearCompletedTodo}
+                filter={this.state.filter}
+                toggleFilter={this.toggleFilter}
+                count={this.getUndoneTodo()} />
+            </Wrapper>
+        )
+    }
+}
+
+const Wrapper = styled.section`
+  background: #fff;
+	margin: 60px 0 40px 0;
+	position: relative;
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+`;
