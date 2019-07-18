f1a748028a44634fba000c00ff1054cc42b6c543
Tugay Serinken <tugay.serinken@hotmail.de>
2019-07-18 20:50:48 +0700

    refactoring app.js and giving some comments

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,47 +1,53 @@
-import React, { Component } from 'react'
-import Todos from './components/Todos';
-import styled from 'styled-components'
-import ActionBar from './components/ActionBar';
-import InputBar from './components/InputBar';
-import TodosContext from './todos-context';
+import React, { Component, Fragment } from "react";
+import Title from "../Ui/components/Title";
+import Todos from "./components/Todos";
+
+// the order how you import is not clean, you should import from packages first and then from components
+import styled from "styled-components";
+import ActionBar from "./components/ActionBar";
+import InputBar from "./components/InputBar";
+import { TodosContext } from "../Contexts/Todos";
 
 export default class TodoList extends Component {
-    static contextType = TodosContext;
+  static contextType = TodosContext;
 
-    // constructor(props, context) {
-    //     super(props, context);
-    // }
+  // constructor(props, context) {
+  //     super(props, context);
+  // }
 
-    render() {
-        const { createTodo,
-            toggleAllTodo,
-            updateTodo,
-            filteredTodos,
-            clearCompletedTodo,
-            filter,
-            toggleFilter,
-        } = this.context;
-        return (
-            <Wrapper>
-                <InputBar
-                    createTodo={createTodo}
-                    toggleAllTodo={toggleAllTodo} />
-                <Todos
-                    updateTodo={updateTodo}
-                    todos={filteredTodos} />
-                <ActionBar
-                    clearCompletedClick={clearCompletedTodo}
-                    filter={filter}
-                    toggleFilter={toggleFilter}
-                    count={this.context.getUndoneTodo()} />
-            </Wrapper>
-        )
-    }
+  render() {
+    // 1 of the reason why whe use context api is because we dont need to receive props and give to components again
+    // you could just use TodoContext directly in each component and dont need to do this way anymore
+    const {
+      createTodo,
+      toggleAllTodo,
+      updateTodo,
+      filteredTodos,
+      clearCompletedTodo,
+      filter,
+      toggleFilter
+    } = this.context;
+    return (
+      <Fragment>
+        <Title>todos</Title>
+        <Wrapper>
+          <InputBar createTodo={createTodo} toggleAllTodo={toggleAllTodo} />
+          <Todos updateTodo={updateTodo} todos={filteredTodos} />
+          <ActionBar
+            clearCompletedClick={clearCompletedTodo}
+            filter={filter}
+            toggleFilter={toggleFilter}
+            count={this.context.getUndoneTodo()}
+          />
+        </Wrapper>
+      </Fragment>
+    );
+  }
 }
 
 const Wrapper = styled.section`
-    background: #fff;
-	margin: 60px 0 40px 0;
-	position: relative;
-	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+  background: #fff;
+  margin: 60px 0 40px 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
 `;
